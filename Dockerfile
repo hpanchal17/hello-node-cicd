47b7a058db2cb9f132679f7e190600388944f950
hardik panchal <hardikpanchal2130@gmail.com>
2025-03-07 17:14:37 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+#stage 1
+FROM node:18-alpine AS nodeimg
+WORKDIR /app
+COPY package.json package-lock.json ./
+RUN npm install
+
+#stage 2
+FROM node:18-alpine
+WORKDIR /app
+COPY --from=nodeimg /app .
+COPY . .
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express'); // Importing express module 
+  
+const app = express(); // Creating an express object 
+  
+const port = 8000;  // Setting an port for this application 
+  
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Books API!');
+});
+
+// Starting server using listen function 
+app.listen(port, function (err) { 
+   if(err){ 
+       console.log("Error while starting server"); 
+   } 
+   else{ 
+       console.log("Server has been started at "+port); 
+   } 
+}) 
