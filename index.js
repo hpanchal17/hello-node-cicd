c2b157996503d4acaf8739928d50d983accf80b6
hpanchal17 <72650853+hpanchal17@users.noreply.github.com>
2025-03-18 19:22:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 8000;  // Setting an port for this application
   
 
 app.get('/', (req, res) => {
-    res.send('Welcome to CICD Pipeline using Jenkins with node with changes & Pushing ECR and Pulling ECR using github action! and deploy in ECR 1');
+    res.send('Welcome to CICD Pipeline using Jenkins with node with changes & Pushing ECR and Pulling ECR using github action! and deploy in ECR 2');
 
 });
 
